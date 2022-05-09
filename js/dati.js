a02e33fc3bb798a7c4272b2c34cee07116156d98
Enzo <EnzoPiccolo7>
2022-05-09 17:48:57 +0200

    finito

--- js/dati.js
+++ js/dati.js
@@ -3,7 +3,7 @@ var cognome = window.prompt('Inserisci il tuo cognome');
 var eta = window.prompt('Inserisci la tua età');
 var citta = window.prompt('Inserisci il la tua città');
 var email = window.prompt('Inserisci la tua email');
-var tutto = nomi + ' '+ cognome + ' '+eta +' '+citta+' '+ email ;
+var tutto = nomi + ' '+ cognome + 'anni '+eta +'città  '+citta+' '+ email ;
 
 var a = document.getElementById('nome').innerHTML = nomi;
 var b = document.getElementById('cognome').innerHTML = cognome;
